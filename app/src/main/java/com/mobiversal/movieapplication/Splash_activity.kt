ec75b7be4be53032d29d6c4c59c48f0518136387
Cătălan Cătălin <catalin_catalan@yahoo.com>
2020-07-13 14:04:20 +0300

    Ultima modificare ora 14:03 - 13 iul.

--- app/src/main/java/com/mobiversal/movieapplication/Splash_activity.kt
+++ app/src/main/java/com/mobiversal/movieapplication/Splash_activity.kt
@@ -4,8 +4,52 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.mobiversal.movieapplication.actor.ActorsRepository
+import com.mobiversal.movieapplication.actor.FavouriteActor
+import com.mobiversal.movieapplication.genre.Genre
+import com.mobiversal.movieapplication.genre.GenreRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+
+    private var genreRepository = GenreRepository.instance
+    private var actorRepository = ActorsRepository.instance
+
+    private var hasActor = false
+    private var hasGenre = false
+
+    private var selectedGenres: List<Genre> = emptyList()
+    private var selectedActors: List<FavouriteActor> = emptyList()
+
+    private fun getSelectedActors() {
+        GlobalScope.launch(Dispatchers.IO) {
+            selectedActors = actorRepository.getAll()
+            hasActor = true
+            setScreen()
+        }
+    }
+
+    private fun getSelectedGenres() {
+        GlobalScope.launch(Dispatchers.IO) {
+            selectedGenres = genreRepository.getAll()
+            hasGenre = true
+            setScreen()
+        }
+    }
+
+    private fun setScreen(){
+        if(hasActor&&hasGenre){
+            GlobalScope.launch (Dispatchers.IO){
+                if(selectedActors.isNotEmpty() || selectedGenres.isNotEmpty())
+                startActivity(Intent(this@SplashActivity , MenuHamburger::class.java))
+                else
+                startActivity(Intent(this@SplashActivity , MainActivity::class.java))
+            }
+        }
+    }
+
 
     // This is the loading time of the splash screen
     private val SPLASH_TIME_OUT:Long = 3000
@@ -16,7 +60,8 @@ class SplashActivity : AppCompatActivity() {
         Handler().postDelayed({
             // This method will be executed once the timer is over
             // Start your app main activity
-            startActivity(Intent(this, MainActivity::class.java))
+            getSelectedActors()
+            getSelectedGenres()
             // close this activity
             finish()
         }, SPLASH_TIME_OUT)
